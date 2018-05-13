4064d02a564b00c30ba49fc86771378219196671
hahaxixi <he.huang@extremevision.mo>
2018-05-13 21:50:43 +0800

    profile page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
